cd3738f2efd0c274f7a5161664ca0dd505c00540
Elvis <davis-elvis@hotmail.com>
2022-04-06 17:24:07 -0500

    proyecto

---
+++ modelos/Usuario.php
@@ -0,0 +1,21 @@
+<?php
+//Incluimos inicialmente la conexion a la base de datos
+require "../config/Conexion.php";
+
+Class Usuario{
+
+    //cramos un constructor vacio 
+
+    public function __construct()
+    {
+
+    {
+        //implememtamos la funcion insertar
+        
+    
+
+
+
+}
+
+?>
